7fe0b79114110c90205311a874ba41ca0e80913b
TEO CHING SIANG <chingsiangteo@gmail.com>
2025-01-03 07:55:11 +0800

    Main Update

---
+++ footer.js
@@ -0,0 +1,5 @@
+document.write(`
+    <footer>
+      <p>&copy; 2025 School Policies and Guidelines. All rights reserved.</p>
+    </footer>
+    `)
---
+++ header.js
@@ -0,0 +1,26 @@
+// header.js
+document.write(`
+    <header>
+        <h1>Welcome to Maple International School Policies and Guidelines</h1>
+        <p>Your one-stop resource for all school rules, regulations, and information.</p>
+    </header>
+
+     <nav>
+        <a href="index.html">Home</a>
+        <div class="dropdown">
+            <a href="#">Sections</a>
+            <div class="dropdown-content">
+                <a href="section-a.html">A: Respect for Authorities</a>
+                <a href="section-b.html">B: Attendance and Punctuality</a>
+                <a href="section-c.html">C: Use of Technology</a>
+                <a href="section-d.html">D: Academic Integrity</a>
+                <a href="section-e.html">E: Properties and Environment</a>
+                <a href="section-f.html">F: Extracurricular Participation</a>
+                <a href="section-g.html">G: Dress Code</a>
+                <a href="section-h.html">H: Prohibited Items and Offenses</a>
+            </div>
+        </div>
+    </nav>
+`);
+
+
